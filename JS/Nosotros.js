216ecae9da5907e57810c52bd686a03eccfb1079
John <ballarinijonathan@gmail.com>
2025-03-05 12:38:50 -0300

    Actualizados los banners y footer
    Former-commit-id: e083b32b9d7bb093aa3231e671df085f4a213f1e [formerly 16bd9d8a637cc49fb272086aa92929a9ef829d19]
    Former-commit-id: 90bf415403510d97472b2bea76f6c3c4c187a314

--- JS/Nosotros.js
+++ JS/Nosotros.js
@@ -3,19 +3,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
     links.forEach(link => {
         link.addEventListener("click", function(event) {
-            event.preventDefault();
-            const sectionId = this.getAttribute("href").substring(1);
-            const section = document.getElementById(sectionId);
-            
-            if (section) {
-                window.scrollTo({
-                    top: section.offsetTop - 20,
-                    behavior: "smooth"
-                });
+            const href = this.getAttribute("href");
+            if (href.startsWith("#")) {
+                event.preventDefault();
+                const sectionId = href.substring(1);
+                const section = document.getElementById(sectionId);
+                
+                if (section) {
+                    window.scrollTo({
+                        top: section.offsetTop - 20,
+                        behavior: "smooth"
+                    });
+                }
             }
         });
     });
 });
+
 
 
 function isInView(element) {
